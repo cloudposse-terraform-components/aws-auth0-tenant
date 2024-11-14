8b07bb65c5489264643c3ecdda5d962763600eed
Igor Rodionov <goruha@gmail.com>
2024-11-14 20:18:35 +0100

    Initial commit

--- src/versions.tf
+++ src/versions.tf
@@ -1,5 +1,14 @@
 terraform {
   required_version = ">= 1.0.0"
 
-  required_providers {}
+  required_providers {
+    auth0 = {
+      source  = "auth0/auth0"
+      version = ">= 1.0.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.9.0"
+    }
+  }
 }
